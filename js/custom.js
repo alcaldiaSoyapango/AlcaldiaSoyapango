d922ce5e56c63b46f2272d96cde57d4e064a14c3
Manuel Bolaños <manu20188marcia@gmail.com>
2020-04-28 19:05:02 -0600

    Prueba para subir pagina

--- js/custom.js
+++ js/custom.js
@@ -58,15 +58,7 @@
       nav: false,
     });
   }
-  // menu fixed js code
-  $(window).scroll(function () {
-    var window_top = $(window).scrollTop() + 1;
-    if (window_top > 50) {
-      $('.main_menu').addClass('menu_fixed animated fadeInDown');
-    } else {
-      $('.main_menu').removeClass('menu_fixed animated fadeInDown');
-    }
-  });
+  
 
 //   $(document).ready(function(){
 
